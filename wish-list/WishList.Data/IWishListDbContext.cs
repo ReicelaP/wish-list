6a410bcc1a0ab96d28443e03679c5e367d43d0fd
Reicela <reicelapetersone@gmail.com>
2022-10-14 11:59:16 +0300

    Added DbContext

---
+++ wish-list/WishList.Data/IWishListDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Threading.Tasks;
+using WishList.Core.Models;
+
+namespace WishList.Data
+{
+    public interface IWishListDbContext
+    {
+        DbSet<T> Set<T>() where T : class;
+        EntityEntry<T> Entry<T>(T entity) where T : class;
+        public DbSet<Wish> Wishes { get; set; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ wish-list/WishList.Data/WishListDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Core.Models;
+
+namespace WishList.Data
+{
+    public class WishListDbContext : DbContext
+    {
+        public WishListDbContext(DbContextOptions options) : 
+            base(options) { }
+
+        public DbSet<Wish> Wishes { get; set; }
+    }
+}
