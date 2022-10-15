46da2d963259b21b8bef7b53fbeab016fca43795
Reicela <reicelapetersone@gmail.com>
2022-10-15 23:14:43 +0300

    added UserApiController

---
+++ wish-list/wish-list/Controllers/UserApiController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WishList.Core.Models;
+using WishList.Services;
+
+namespace wish_list.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class UserApiController : ControllerBase
+    {
+        private UserService _userService;
+
+        public UserApiController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [Route("names")]
+        [HttpPost]
+        public IActionResult GetNames(UserList userList)
+        {
+            var names = _userService.GetNames(userList);
+
+            if (names == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(names);
+        }
+    }
+}
