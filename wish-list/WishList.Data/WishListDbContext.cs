94433da6781483f071357cff03163005c7e5e217
Reicela <reicelapetersone@gmail.com>
2022-10-15 23:16:28 +0300

    updated DbContext

--- wish-list/WishList.Data/WishListDbContext.cs
+++ wish-list/WishList.Data/WishListDbContext.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 using WishList.Core.Models;
 
 namespace WishList.Data
 {
-    public class WishListDbContext : DbContext
+    public class WishListDbContext : DbContext, IWishListDbContext
     {
         public WishListDbContext(DbContextOptions options) : 
             base(options) { }
 
         public DbSet<Wish> Wishes { get; set; }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
