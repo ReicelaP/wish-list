0b15bc5e86f77fa375d504f4a47d9e911bb21b59
Reicela <reicelapetersone@gmail.com>
2022-10-16 01:39:51 +0300

    wish list tests

---
+++ wish-list/WishList.Tests/WishTitleValidatorTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WishList.Core.Models;
+using WishList.Core.Validations;
+
+namespace WishList.Tests
+{
+    [TestClass]
+    public class WishTitleValidatorTests
+    {
+        [TestMethod]
+        public void IsValid_HaveValue_ReturnTrue()
+        {
+            //Act
+            var wish = new Wish();
+            wish.Title = "Ticket";
+
+            //Assert
+            var result = WishTitleValidator.IsValid(wish);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsValid_NullOrEmpty_ReturnFalse()
+        {
+            //Act
+            var wish = new Wish();
+            wish.Title = "";
+
+            //Assert
+            var result = WishTitleValidator.IsValid(wish);
+
+            //Assert
+            result.Should().BeFalse();
+        }
+    }
+}
